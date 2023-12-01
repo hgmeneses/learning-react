d4d28b9aaa69f0fe673805103f977df64d3536c0
Gabriel Meneses <hector.meneses@globant.com>
2023-12-01 16:35:12 -0500

    learning functional component and arrow functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 
-//const name = 'Gabriel Meneses'
-
-const user = {
-    name: 'Gabriel Meneses',
-    age: 28,
-    country: 'Colombia'
+const TarjetaFruta = () => {
+    return (
+        <div>
+          <h3>Titulo</h3>
+          <hr/>
+          <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit</p>
+        </div>
+    )
 }
 
-function getInfo(user) {
-    return `Hola mi nombre es ${user.name}, soy de ${user.country} y tengo ${user.age} de edad.`
-}
-const App = <h1>{ getInfo(user) }</h1>
-// const App = React.createElement("h1", null, "Hola React")
-//
+const TarjetaVerdura = () => (
+    <div>
+        <h3>Titulo</h3>
+        <hr/>
+        <p>Descripcion ... </p>
+    </div>
+)
 
-ReactDOM.render(App, document.getElementById('root'))
+const App = () => (
+    <div>
+      <TarjetaFruta />
+      <TarjetaVerdura />
+
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
