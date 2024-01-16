29b5ffc75333e5c04c4542409160b4d53c63a2a9
Hector G Meneses <hector.meneses@globant.com>
2024-01-16 18:21:06 -0500

    add props to arrow functions and how to render this props on the component

--- src/index.js
+++ src/index.js
@@ -11,19 +11,22 @@ const TarjetaFruta = () => {
     )
 }
 
-const TarjetaVerdura = () => (
+const TarjetaVerdura = (props) => (
     <div>
-        <h3>Titulo</h3>
+        <h3>{ props.name }</h3>
         <hr/>
-        <p>Descripcion ... </p>
+        <p>$ { props.price} </p>
     </div>
 )
+
+let verduraRara = 'Zanahoria'
 
 const App = () => (
     <div>
       <TarjetaFruta />
-      <TarjetaVerdura />
-
+      <TarjetaVerdura name={`${verduraRara}`} price={2.00} />
+      <TarjetaVerdura name='Papa' price={1.50} />
+      <TarjetaVerdura name='Arracacha' price={5.00} />
     </div>
 )
 
