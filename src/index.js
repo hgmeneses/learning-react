e4384874c84a215c34a633796f4281a9a2a22166
Hector G Meneses <hector.meneses@globant.com>
2024-01-16 19:35:02 -0500

    removed unused component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 
-const TarjetaFruta = () => {
-    return (
-        <div>
-          <h3>Titulo</h3>
-          <hr/>
-          <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit</p>
-        </div>
-    )
-}
-
 const TarjetaVerdura = (props) => (
     <div>
         <h3>{ props.name }</h3>
