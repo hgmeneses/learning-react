552ea9714379b073d42d7c540a2c31ab8be55ba3
Hector G Meneses <hector.meneses@globant.com>
2024-01-24 22:13:53 -0500

    create my first class component and use its state to change increase the cantidad value

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 
+/*
+ Esto es un componente de clase, y tenemos un constructor que simplemente pasa
+ hace un super(esto diciendo que no va a modificar el constructor)
+ luego de eso agrega un nuevo "atributo" al objeto `state` llamado
+ cantidad, el cual se ira actualizando usando this.setState.
+ Esto succede a una funcion onClick dentro de un atributo de <button>
+*/
+class TarjetaFruta extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      cantidad: 0
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h3>{ this.props.name }</h3>
+        <div> Cantidad: { this.state.cantidad }</div>
+        <button
+          onClick={ () => {
+            this.setState({
+              cantidad: this.state.cantidad + 1
+            })
+          }}
+        >
+          Agregar
+        </button>
+        <hr/>
+        <p>$ { this.props.price} </p>
+      </div>
+    )
+  }
+}
+
 const TarjetaVerdura = (props) => (
     <div>
         <h3>{ props.name }</h3>
@@ -10,13 +46,17 @@ const TarjetaVerdura = (props) => (
 )
 
 let verduraRara = 'Zanahoria'
+let frutaRara = 'Feijoa'
 
 const App = () => (
     <div>
-      <TarjetaFruta />
       <TarjetaVerdura name={`${verduraRara}`} price={2.00} />
       <TarjetaVerdura name='Papa' price={1.50} />
       <TarjetaVerdura name='Arracacha' price={5.00} />
+
+      <TarjetaFruta name={`${frutaRara}`} price={9.00} />
+      <TarjetaFruta name='Manzana' price={1.50} />
+      <TarjetaFruta name='Sandia' price={5.00} />
     </div>
 )
 
