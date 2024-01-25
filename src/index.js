ca63887e97eb3a5e12287c4f025c03169bfc98d7
Hector G Meneses <hector.meneses@globant.com>
2024-01-24 23:01:03 -0500

    learn how to use bind to pass this context of the class through a methods

--- src/index.js
+++ src/index.js
@@ -12,24 +12,64 @@ class TarjetaFruta extends React.Component {
   constructor () {
     super()
 
+    /*o tambien podemos agregar el llamado desde el constructor
+      lo que a mi parecer le da un acabado mas limpio, Dejamos en la
+      funcion `onClick` el llamado a this.agregar solamente.
+
+      Esto es funcional con algunos bind pero podemos hacerlo aun mas chevere
+      this.agregar = this.agregar.bind(this)
+      this.restar = this.restar.bind(this)
+    */
+
+    /* Creamos un array para agregar nuestros metodos */
+    const METHODS = [
+      'agregar',
+      'restar',
+      'limpiar'
+    ]
+
+    /* Luego recorremos el array y a cada metodo le hacemos el bind(this) */
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this)
+    })
+
     this.state = {
       cantidad: 0
     }
   }
+
+  agregar () {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  restar () {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar () {
+    this.setState({
+      cantidad: 0
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>{ this.props.name }</h3>
-        <div> Cantidad: { this.state.cantidad }</div>
-        <button
-          onClick={ () => {
-            this.setState({
-              cantidad: this.state.cantidad + 1
-            })
-          }}
-        >
-          Agregar
-        </button>
+        <div> Cantidad: { this.state.cantidad } </div>
+        {/*Esta es una forma de decirle a js que use this de la clase
+           y no del metodo en si: `.bind(this)` sin agregar nada al
+           constructor de la clase.
+
+           onClick={ this.agregar.bind(this) }
+         */}
+        <button onClick={ this.agregar } > + </button>
+        <button onClick={ this.restar } > - </button>
+        <button onClick={ this.limpiar} > 0 </button>
         <hr/>
         <p>$ { this.props.price} </p>
       </div>
