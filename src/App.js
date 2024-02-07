c5769ff5b4e55d7e0709ce06a286330ff0350d3d
Hector G Meneses <hector.meneses@globant.com>
2024-02-06 20:22:21 -0500

    remove constant calls in App to use only the dynamic component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React from 'react'
 import TarjetaFruta from './components/TarjetaFruta'
 import TarjetaVerdura from './components/TarjetaVerdura'
 
-let frutaRara = 'Feijoa' /* Realmente esto no es necesario pero siguiendo el ejemplo voy a poderlo aca y luego eliminarlo */
-let verduraRara = 'Zanahoria' /* Realmente esto no es necesario pero siguiendo el ejemplo voy a poderlo aca y luego eliminarlo */
-
 const App = () => (
 	<div>
-		<TarjetaVerdura name={`${verduraRara}`} price={2.0} />
 		<TarjetaVerdura name="Papa" price={1.5} />
 		<TarjetaVerdura name="Arracacha" price={5.0} />
 
-		<TarjetaFruta name={`${frutaRara}`} price={9.0} />
 		<TarjetaFruta name="Manzana" price={1.5} />
 		<TarjetaFruta name="Sandia" price={5.0} />
 	</div>
