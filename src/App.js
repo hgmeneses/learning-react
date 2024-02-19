842a6f3b9bfc1f627ace66616edca513aacc7faf
Hector G Meneses <hector.meneses@globant.com>
2024-02-19 15:55:45 -0500

    convert App from functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TarjetaFruta from './components/TarjetaFruta'
 import TarjetaVerdura from './components/TarjetaVerdura'
 import Contador from './components/Contador/Contador'
@@ -9,28 +9,33 @@ const otrosDatos = {
     peleasNocturnas: 300
 }
 
-const masAtributos = {
-    fuerza: 100,
-    vidasRestantes: 7,
-    name: 'Pirry' /* Recordemos que si ejecutamos esta linea al estar haciendo untado de objetos sobreescribira el nombre que estamos pasando primero en las props */
+
+class App extends Component {
+
+    state =  {
+        fuerza: 100,
+        vidasRestantes: 7,
+        name: 'Pirry' /* Recordemos que si ejecutamos esta linea al estar haciendo untado de objetos sobreescribira el nombre que estamos pasando primero en las props */
+    }
+
+    render() {
+        return (
+            <div>
+                <TarjetaVerdura name="Papa" price={1.5} />
+                <TarjetaVerdura name="Arracacha" price={5.0} />
+
+                <TarjetaFruta name="Manzana" price={1.5} />
+                <TarjetaFruta name="Sandia" price={5.0} />
+                <Contador />
+                <Gato
+                name='Garfield'
+                age='2 años'
+                { ...otrosDatos }
+                { ...this.state }
+                />
+            </div>
+        )
+    }
 }
-
-const App = () => (
-
-	<div>
-		<TarjetaVerdura name="Papa" price={1.5} />
-		<TarjetaVerdura name="Arracacha" price={5.0} />
-
-		<TarjetaFruta name="Manzana" price={1.5} />
-		<TarjetaFruta name="Sandia" price={5.0} />
-        <Contador />
-        <Gato
-            name='Garfield'
-            age='2 años'
-            { ...otrosDatos }
-            { ...masAtributos }
-        />
-	</div>
-)
 
 export default App
